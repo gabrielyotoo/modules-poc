3d5384a43175a3e82b0b0dc2a515e021a4e1cac5
Gabriel Freitas Yamamoto <gabrielyamamoto82@gmail.com>
2023-06-19 18:24:14 -0300

    feat: add components workspace

---
+++ packages/components/src/input/input.tsx
@@ -0,0 +1,5 @@
+import { TextInput, TextInputProps } from "react-native";
+
+const Input = (props: TextInputProps) => <TextInput {...props} />;
+
+export default Input;
--- packages/main/src/MainNavigator.tsx
+++ packages/main/src/MainNavigator.tsx
@@ -1,10 +1,12 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text } from "react-native";
+import React from "react";
+import { Input } from "components";
 
 const MainNavigator = () => {
   return (
     <View>
       <Text>MainNavigator</Text>
+      <Input />
     </View>
   );
 };
